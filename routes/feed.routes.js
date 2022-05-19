6d87a07323cf4180ebdae06d0d817a7f4146a877
hmarchbank <93050431+hmarchbank@users.noreply.github.com>
2022-05-19 15:57:15 +0200

    added feed-routes

---
+++ routes/feed.routes.js
@@ -0,0 +1,128 @@
+const router = require("express").Router();
+
+const { default: mongoose } = require("mongoose");
+const Feed = require('../models/Feed.model');
+// const  { isAuthenticated } = require("../middleware/jwt.middleware")
+
+//CREATE POSTS
+
+//NEEDS ROUTE GUARD
+
+router.post('/feed', (req, res, next) => {
+    const { title, content, timestamps, postedBy } = req.body;
+
+    const newPost = {
+        title, 
+        content, 
+        postedBy, 
+        timestamps
+    }
+
+    Feed.create(newPost)
+        .then(response => res.status(201).json(response))
+        .catch(err => {
+            console.log("error creating a new post", err);
+            res.status(500).json({
+                message: "error creating a new post",
+                error: err
+            });
+        })
+});
+
+// GET LIST OF POSTS
+
+// UNPROTECTED ROUTE
+
+router.get("/feed", (req, res, next) => {
+    Feed.find()
+        .then(response => {
+            res.json(response)
+        })
+        .catch(err => {
+            console.log("error getting list of posts", err);
+            res.status(500).json({
+                message: "error getting list of posts",
+                error: err
+            });
+        })
+});
+
+//  GET DETAILS FOR ONE POST
+
+
+// UNPROTECTED ROUTE
+
+router.get('/feed/:postId', (req, res, next) => {
+    
+    const { postId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(postId)) {
+        res.status(400).json({ message: 'Specified id is not valid' });
+        return;
+    }
+
+    Feed.findById(postId)
+        // .populate('tasks')
+        .then(post => res.json(post))
+        .catch(err => {
+            console.log("error getting details of a post", err);
+            res.status(500).json({
+                message: "error getting details of a post",
+                error: err
+            });
+        })
+});
+
+// UPDATE POST BY ID
+// NOT IN MVP BUT ROUTE IS HERE AND WORKS IF WE HAVE TIME
+// NEEDS ROUTE GUARD
+
+router.put('/feed/:postId', (req, res, next) => {
+    const { postId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(postId)) {
+        res.status(400).json({ message: 'Specified id is not valid' });
+        return;
+    }
+
+    Feed.findByIdAndUpdate(postId, req.body, { new: true })
+        .then((updatedPost) => res.json(updatedPost))
+        .catch(err => {
+            console.log("error updating event", err);
+            res.status(500).json({
+                message: "error updating event",
+                error: err
+            });
+        })
+});
+
+
+// DELETE A POSTS
+// NOT IN MVP BUT ROUTE IS HERE AND WORKS IF WE HAVE TIME
+// NEEDS ROUTE GUARD
+
+router.delete('/feed/:postId', (req, res, next) => {
+    const { postId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(postId)) {
+        res.status(400).json({ message: 'Specified id is not valid' });
+        return;
+    }
+
+    Feed.findByIdAndRemove(postId)
+        .then((response) => {
+            console.log(response)
+            res.json({
+                message: `The post: ${response.title} was removed successfully.` })})
+        .catch(err => {
+            console.log("error deleting event", err);
+            res.status(500).json({
+                message: "error deleting event",
+                error: err
+            });
+        })
+});
+
+
+
+module.exports = router;

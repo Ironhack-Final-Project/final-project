59bfba824efd850d771492423b75bdcbf8871b9c
Valentin Paul <valentin@vamptech.de>
2022-05-19 16:32:40 +0200

    added jwt middleware

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 
-// const {isAuthenticated} = require("../middleware/jwt.middleware");
+const {isAuthenticated} = require("../middleware/jwt.middleware");
 const User = require("../models/User.model");
 
 const router = express.Router();
@@ -122,16 +122,16 @@ router.post('/login', (req, res, next) => {
 
 
 // Verify
-// router.get('/verify', isAuthenticated, (req, res, next) => {
+router.get('/verify', isAuthenticated, (req, res, next) => {
  
-//     // If JWT token is valid the payload gets decoded by the
-//     // isAuthenticated middleware and made available on `req.payload`
-//     console.log("token is valid", req.payload);
-//     console.log("req.payload...", req.payload);
+    // If JWT token is valid the payload gets decoded by the
+    // isAuthenticated middleware and made available on `req.payload`
+    console.log("token is valid", req.payload);
+    console.log("req.payload...", req.payload);
     
-//     // Send back the object with user data
-//     // previously set as the token payload
-//     res.status(200).json(req.payload);
-//   });
+    // Send back the object with user data
+    // previously set as the token payload
+    res.status(200).json(req.payload);
+  });
 
 module.exports = router;

459c183bd18c4c1dfad2662104d724fba7306e9c
hmarchbank <93050431+hmarchbank@users.noreply.github.com>
2022-05-24 15:47:29 +0200

    pushing auth.routes to github

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 
-const {isAuthenticated} = require("../middleware/jwt.middleware");
+const { isAuthenticated } = require("../middleware/jwt.middleware");
 const User = require("../models/User.model");
 
 const router = express.Router();
@@ -11,38 +11,38 @@ let isAdmin = false
 
 // Create Account
 router.post('/signup', (req, res, next) => {
-    const { email, password, username, imageUrl, adminKey } = req.body;
+  const { email, password, username, imageUrl, adminKey } = req.body;
 
-    // Check if email or password or name are provided as empty string 
-    if (email === '' || password === '' || username === '') {
-      res.status(400).json({ message: "Provide email, password and name" });
-      return;
-    }
+  // Check if email or password or name are provided as empty string 
+  if (email === '' || password === '' || username === '') {
+    res.status(400).json({ message: "Provide email, password and name" });
+    return;
+  }
 
-    // Use regex to validate the email format
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
-    if (!emailRegex.test(email)) {
-      res.status(400).json({ message: 'Provide a valid email address.' });
-      return;
-    }
+  // Use regex to validate the email format
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+  if (!emailRegex.test(email)) {
+    res.status(400).json({ message: 'Provide a valid email address.' });
+    return;
+  }
 
-    if (adminKey === '7'){
-      isAdmin = true
-    }
+  if (adminKey === '7') {
+    isAdmin = true
+  }
 
-    // Use regex to validate the password format
-    const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-    if (!passwordRegex.test(password)) {
-      res.status(400).json({ message: 'Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.' });
-      return;
-    }
-    // Check the users collection if a user with the same email already exists
+  // Use regex to validate the password format
+  const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!passwordRegex.test(password)) {
+    res.status(400).json({ message: 'Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.' });
+    return;
+  }
+  // Check the users collection if a user with the same email already exists
   User.findOne({ username })
     .then((foundUser) => {
       // If the user with the same email already exists, send an error response
       if (foundUser) {
         return res.status(400).json({ message: "User already exists." });
-         
+
       }
 
       // If email is unique, proceed to hash the password
@@ -106,12 +106,12 @@ router.post('/login', (req, res, next) => {
         const { _id, username, isAdmin, imageUrl } = foundUser;
 
         // Create an object that will be set as the token payload
-        const payload = { 
-          _id, 
+        const payload = {
+          _id,
           username,
           isAdmin,
           imageUrl
-         };
+        };
 
         // Create and sign the token
         const authToken = jwt.sign(
@@ -136,56 +136,79 @@ router.post('/login', (req, res, next) => {
 
 // Verify
 router.get('/verify', isAuthenticated, (req, res, next) => {
- 
-    // If JWT token is valid the payload gets decoded by the
-    // isAuthenticated middleware and made available on `req.payload`
-    console.log("token is valid", req.payload);
-    console.log("req.payload...", req.payload);
-    
-    // Send back the object with user data
-    // previously set as the token payload
-    res.status(200).json(req.payload);
-  });
 
-  // Add dogs
+  // If JWT token is valid the payload gets decoded by the
+  // isAuthenticated middleware and made available on `req.payload`
+  console.log("token is valid", req.payload);
+  console.log("req.payload...", req.payload);
 
-  router.put('/user/:userId/add-dog', (req, res, next) => {
-    console.log(req.body)
-    let newDog = {
+  // Send back the object with user data
+  // previously set as the token payload
+  res.status(200).json(req.payload);
+});
+
+// Add dogs
+
+router.put('/user/:userId/add-dog', (req, res, next) => {
+  console.log(req.body)
+  let newDog = {
+    name: req.body.name,
+    breed: req.body.breed,
+    imageUrl: req.body.imageUrl
+  }
+
+  if (newDog.imageUrl === '') {
+    newDog = {
       name: req.body.name,
       breed: req.body.breed,
-      imageUrl: req.body.imageUrl
     }
+  }
 
-    if (newDog.imageUrl === ''){
-      newDog = {
-        name: req.body.name,
-        breed: req.body.breed,
-      }
-    }
+  console.log(newDog)
 
-    console.log(newDog)
+  User.findByIdAndUpdate(req.params.userId, { $push: { dogs: newDog } })
+    .populate("dogs")
+    .then((response) => {
+      res.json(response)
+    })
+    .catch()
+})
 
-    User.findByIdAndUpdate(req.params.userId, { $push: {dogs: newDog }})
-      .populate("dogs")
-      .then((response) => {
-        res.json(response)
+router.get('/user/:userId/add-dog', (req, res, next) => {
+  User.findById(req.params.userId)
+    .populate("dogs")
+    .then(response => {
+      res.json(response)
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'error getting dogs',
+        error: err
       })
-      .catch()
-  })
+    })
+})
 
-  router.get('/user/:userId/add-dog', (req, res, next) => {
-    User.findById(req.params.userId)
-      .populate("dogs")
-      .then(response => {
-        res.json(response)
+router.get('/user/:userId', (req, res, next) => {
+  console.log(req.params.userId)
+  User.findById(req.params.userId)
+    .then(response => { res.json(response) })
+    .catch(err => {
+      res.status(500).json({
+        message: 'error deleting dog',
+        error: err
       })
-      .catch(err => {
-        res.status(500).json({
-          message: 'error getting dogs',
-          error: err
-        })
+    })
+})
+
+router.put('/user/:userId', (req, res, next) => {
+  User.findByIdAndUpdate(req.body._id, req.body)
+    .then(response => { res.json(response) })
+    .catch(err => {
+      res.status(500).json({
+        message: 'error deleting dog',
+        error: err
       })
-  })
+    })
+})
 
 module.exports = router;
